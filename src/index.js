7de310848388432bdcf4234116a476fa06fd175f
Jaid <jaid.jsx@gmail.com>
2019-06-30 07:59:03 +0200

    feat: Implemented basic functionality
    Signed-off-by: Jaid <jaid.jsx@gmail.com>

--- src/index.js
+++ src/index.js
@@ -1,11 +1,41 @@
 /** @module resolve-any */
 
+import {isFunction} from "lodash"
+import isPromise from "p-is-promise"
+
 /**
- * Returns the number of seconds passed since Unix epoch (01 January 1970)
+ * Returns given value. If `value` is a function, returns its return value. If `value` is a `Promise`, returns its resolved value.
  * @example
  * import resolveAny from "resolve-any"
- * const result = resolveAny()
- * result === 1549410770
- * @returns {number} Seconds since epoch
+ * const result = resolveAny("hello")
+ * result === "hello"
+ * import resolveAny from "resolve-any"
+ * const result = resolveAny(() => "world")
+ * result === "world"
+ * @async
+ * @param value
+ * @returns {*} Resolved value
  */
-export default () => Math.floor(Date.now() / 1000)
+export default async value => {
+  const returnedValue = isFunction(value) ? value() : value
+  return returnedValue
+}
+
+/**
+ * Returns given value. If `value` is a function, returns its return value.
+ * @example
+ * import {resolveSync} from "resolve-any"
+ * const result = resolveSync("hello")
+ * result === "hello"
+ * import {resolveSync} from "resolve-any"
+ * const result = resolveSync(() => "world")
+ * result === "world"
+ * @param value
+ * @returns {*} Resolved value
+ */
+export const resolveSync = value => {
+  if (value |> isFunction) {
+    return value()
+  }
+  return value
+}
--- test/test.js
+++ test/test.js
@@ -1,9 +1,21 @@
 import path from "path"
 
 const indexModule = (process.env.MAIN ? path.resolve(process.env.MAIN) : path.join(__dirname, "..", "src")) |> require
-const {default: resolveAny} = indexModule
+const {default: resolveAny, resolveSync} = indexModule
 
-it("should run", () => {
-  const result = resolveAny()
-  expect(result).toBeGreaterThan(1549410770)
+const asyncValue = async () => {
+  return 1
+}
+
+it("should run sync", () => {
+  expect(resolveSync(1)).toStrictEqual(1)
+  expect(resolveSync(() => 1)).toStrictEqual(1)
+})
+
+it("should run async", async () => {
+  expect(await resolveAny(1)).toStrictEqual(1)
+  expect(await resolveAny(() => 1)).toStrictEqual(1)
+  expect(await resolveAny(asyncValue)).toStrictEqual(1)
+  expect(await resolveAny(asyncValue())).toStrictEqual(1)
+  expect(await resolveAny(() => asyncValue())).toStrictEqual(1)
 })
